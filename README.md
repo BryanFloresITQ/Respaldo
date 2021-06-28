d05dc8a9762253a42a0c7dad94d71752080ea8b6
Bryan Flores <bryancrack016@gmail.com>
2021-06-28 12:29:09 -0500

    Respaldo acabado V,:

---
+++ README.md
@@ -0,0 +1,10 @@
+## Crear un router en la carpeta `routes` que contenga
+
+-   Un Endpoint que permita receptar un id en los parametros y que retorno un personaje
+-   Un Endpoint que permita receptar un array de ids en la consulta y que retorne unicamente los personajes que coincidan con los ids
+-   Un Endpoint que permita receptar un numero de pagina en la consulta y retorno los personajes de dicha pagina
+-   Un Endpoint que permita receptar el genero y un numero de pagina (pagina es opcional) y que retorne los personajes que coincidan
+
+### La estructura de respuesta es:
+
+`{ msg: "Un mensaje", body: [] ò {} }`
---
+++ routes/index.js
@@ -0,0 +1,128 @@
+const { Router } = require('express');
+const { FetchData } = require('../lib/data');
+
+
+//Inicializamos Router
+const router = Router();
+
+//Creacion de los endpoint
+router
+    .get("/personajes-parametro/:id",(req, res) => {
+        var filtrarID 
+
+        const { params: {id} } = req;
+    
+        (async () => {
+
+            try {
+                filtrarID = await FetchData.getCharacter(id);
+        
+            
+
+        if (id == null){
+            res.json({
+                 msg: "No se ha seleccionado el ID", 
+                 body: filtrarID
+            })
+        }else{
+
+            res.json({
+                msg: "Personajes Filtrados por ID",
+                body:[filtrarID]
+            })
+
+        }
+        res.end();
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})()
+    })
+    .get('/arreglo-ids', (req, res) => {
+
+        (async () => {
+
+            try {
+
+                var FiltrarIdes;
+
+                const { query: {ids} } = req;
+
+                var ides = ids.split(",");
+
+                FiltrarIdes = await FetchData.getSpecificCharacters(ides)
+                resolve(FiltrarIdes);
+
+                res.json({
+                    msg: "Personajes Filtrados por ID",
+                    body:[filtrarID]
+                })
+        
+            } catch (error) {
+                console.log(error);
+            }
+
+        })()
+    })
+    .get("/pagina",(req, res) => {
+        
+    
+        (async () => {
+
+            try {
+
+                const { query: {pagina} } = req;
+
+                var filtrarPagina = await FetchData.getByPage(pagina);
+
+                res.json({
+                    msg: "Personajes Filtrados por Pagina",
+                    body:[filtrarPagina]
+                })
+    
+                res.end();
+        
+            } catch (error) {
+                console.log(error);
+            }
+        
+        })()
+
+        
+    })
+    .get("/genero",(req, res) => {
+        var filtrarGenero
+
+        const { query: {genero,pagina} } = req;
+    
+        (async () => {
+
+            try {
+                filtrarGenero = await FetchData.getByCharacterGender(genero, pagina);
+
+                if (genero == null){
+                    res.json({
+                         msg: "No se ha encontrado coincidencias", 
+                         body: [] 
+                    })
+                }else{
+        
+                    res.json({
+                        msg: "Personajes Filtrados por Pagina",
+                        body:[filtrarGenero]
+                    })
+        
+                }
+        
+            } catch (error) {
+                console.log(error);
+            }
+        
+        })()
+
+    })
+
+//Exportamos las rutas
+module.exports.RouterIndex = router;
